c027f904a265bb0ed82d6ac98b61084751179d85
dereknoblej <derek.noblej@gmail.com>
2019-06-03 13:53:28 -0500

    first exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+drop table if exists albums;
+
+create table albums(
+id int unsigned not null auto_increment,
+artist varchar(255) not null,
+name  varchar(255) not null,
+release_date int not null,
+sales decimal(10, 2),
+genre varchar(255) not null,
+primary key(id)
+);
