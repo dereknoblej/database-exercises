c4311911310ea84a9523ebcd97fd6c1f701b9425
dereknoblej <derek.noblej@gmail.com>
2019-06-04 14:07:44 -0500

    WHERE exercise

---
+++ where_exercises.sql
@@ -0,0 +1,21 @@
+use employees;
+
+# select *
+# from employees
+# where first_name IN ('Irena', 'Vidya', 'Maya');
+
+# select *
+# from employees
+# where last_name LIKE 'E%';
+
+# select *
+# from employees
+# where hire_date LIKE '199%';
+
+# select *
+# from employees
+# where birth_date LIKE '%-12-25';
+
+# select *
+# from employees
+# where last_name LIKE '%q%';
