6ea6280f86fb76595a67a48c5db1a42d1181caf0
dereknoblej <derek.noblej@gmail.com>
2019-06-04 14:31:12 -0500

    Complete Where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,7 +2,10 @@ use employees;
 
 # select *
 # from employees
-# where first_name IN ('Irena', 'Vidya', 'Maya');
+# where gender = 'M' AND (first_name = 'Irena'
+#   OR first_name = 'Vidya'
+#   OR first_name = 'Maya');
+
 
 # select *
 # from employees
@@ -19,3 +22,23 @@ use employees;
 # select *
 # from employees
 # where last_name LIKE '%q%';
+
+# select *
+# from employees
+# where last_name LIKE '%E'
+# AND last_name LIKE 'E%';
+#
+# select *
+# from employees
+# where last_name LIKE '%E'
+#   OR last_name LIKE 'E%';
+
+# select *
+# from employees
+# where hire_date LIKE '199%'
+# AND birth_date LIKE '%-12-25';
+
+select *
+from employees
+where last_name NOT LIKE '%Qu%'
+AND last_name LIKE '%Q%';
