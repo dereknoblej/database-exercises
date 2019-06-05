f42fe37b21bd32b89189fce9f0738ae8a7528bba
dereknoblej <derek.noblej@gmail.com>
2019-06-05 09:11:34 -0500

    Order by Exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,46 @@
+use employees;
+
+select *
+from employees
+where gender = 'M' AND (first_name = 'Irena'
+  OR first_name = 'Vidya'
+  OR first_name = 'Maya')
+ORDER BY last_name ASC,
+first_name ASC;
+
+
+# select *
+# from employees
+# where last_name LIKE 'E%';
+
+# select *
+# from employees
+# where hire_date LIKE '199%';
+
+# select *
+# from employees
+# where birth_date LIKE '%-12-25';
+
+# select *
+# from employees
+# where last_name LIKE '%q%';
+
+# select *
+# from employees
+# where last_name LIKE '%E'
+# AND last_name LIKE 'E%';
+#
+# select *
+# from employees
+# where last_name LIKE '%E'
+#   OR last_name LIKE 'E%';
+
+# select *
+# from employees
+# where hire_date LIKE '199%'
+# AND birth_date LIKE '%-12-25';
+
+# select *
+# from employees
+# where last_name NOT LIKE '%Qu%'
+#   AND last_name LIKE '%Q%';
